01758002ae8813b732c5f84c95fa0080f6da2d9f
Mohammed Yaseen <mohammed.yaseen@kalvium.community>
2024-05-25 11:41:12 +0400

    post api demo is done

--- BACKEND/route.js
+++ BACKEND/route.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { createtask } = require('./handler');
 
 router.get('/test', (req, res) => {
     res.json({ message: 'This is a test route' });
 });
+
+router.post('/task', createtask); 
 
 module.exports = router;
