f4ceb31eb1f5232b1c1f82b529bd882374d2b2a3
Mohammed Yaseen <mohammed.yaseen@kalvium.community>
2024-05-25 10:36:50 +0400

    get demo is done

---
+++ BACKEND/DBconnection.js
@@ -0,0 +1,13 @@
+// const mongoose = require('mongoose')
+// const dotenv = require('dotenv').config()
+
+// const DBconnection = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI);
+//         console.log("DB Connected");
+//     } catch (error) {
+//         console.error(error);
+//     }
+// };
+
+// module.exports = DBconnection
